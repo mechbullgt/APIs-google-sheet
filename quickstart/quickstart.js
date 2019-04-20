e27f7d6ee3f5c51a33ae940a19ed38e82537b7fb
Maqbool Ansari <maqbool.ansari@mindstix.com>
2019-04-20 19:57:30 +0530

    Merged dev changes

--- quickstart/quickstart.js
+++ quickstart/quickstart.js
@@ -83,10 +83,10 @@ function listMajors(auth) {
     if (err) return console.log('The API returned an error: ' + err);
     const rows = res.data.values;
     if (rows.length) {
-      console.log('Name, Major:');
+      // console.log('Name, Major:');
       // Print columns A and E, which correspond to indices 0 and 4.
       rows.map((row) => {
-        console.log(`${row[0]}, ${row[1]}`);
+        console.log(`${row[4]}`);
       });
     } else {
       console.log('No data found.');
